998acbcc85a72c859dc578626af32e3302d7e560
Andrei Diaconu <andreidiaconu88@gmail.com>
2019-06-17 17:54:50 +0300

    final commit

--- src/app/layout/nav/navbar.component.ts
+++ src/app/layout/nav/navbar.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
   playerClicked: boolean;
   skillClicked: boolean;
 
   constructor() {
     this.playerClicked = true;
-    this.skillClicked = false;
   }
 
   togglePlayer() {
-    console.log("11 " + this.playerClicked);
-
-    this.playerClicked = !this.playerClicked;
-    console.log("12 " + this.playerClicked);
+    this.playerClicked = true;
   }
 
   toggleSkill() {
-    console.log("1 " + this.playerClicked);
-    this.skillClicked = !this.skillClicked;
-    console.log("2 " + this.playerClicked);
-  }
-
-  ngOnInit() {
-
+    this.playerClicked = false;
   }
 }
